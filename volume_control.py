e7e684b728571be4f0373ad75f94f5d3c8d139b4
Swarup Mohapatra <1930159@kiit.ac.in>
2021-07-09 02:01:23 +0530

    commit 3

--- volume_control.py
+++ volume_control.py
@@ -9,11 +9,24 @@ wCam, hCam = 1980, 1080
 cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
+pTime = 0
+
+detector = hm.handDetector()
 
 
 while True:
     success, img = cap.read()
-    cv2.imshow("Img", img)
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f'FPS:: {int(fps)}', (30,60), cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 0, 0))
+
+    cv2.imshow("img", img)
     cv2.waitKey(1)
 
 
