5992260fb4831be84d7f08d96f3220fab1b6c6d3
Swarup Mohapatra <1930159@kiit.ac.in>
2021-07-10 03:49:24 +0530

    commit 3

--- volume_control.py
+++ volume_control.py
@@ -18,8 +18,19 @@ while True:
     success, img = cap.read()
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
-    print(lmList)
+    if len(lmList) != 0:
+        print(lmList[4], lmList[8])
 
+        a, b = lmList[4][1], lmList[4][2]
+        x, y = lmList[8][1], lmList[8][2]
+        c1, c2 = (a + x)//2, (b + y)//2
+
+        cv2.circle(img, (a, b), 12, (255, 255, 255), cv2.FILLED)
+        cv2.circle(img, (x, y), 12, (255, 255, 255), cv2.FILLED)
+        cv2.circle(img, (c1, c2), 12, (0, 0, 0), cv2.FILLED)
+
+        length = math.hypot(x - a, y - b)
+        print(length)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
